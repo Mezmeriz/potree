9a1ce81df9ea014e442fdde1eabb762c9a5ac5a6
Robert Pettit <rpettit@mezmeriz.com>
2020-10-02 11:48:56 -0400

    MEZ-877 - Fetch baseurl in a working way

--- src/Potree.js
+++ src/Potree.js
@@ -16,22 +16,7 @@ console.log('Potree ' + version.major + '.' + version.minor + version.suffix);
 
 export const debug = {};
 
-let scriptPath = "";
-if (document.currentScript.src) {
-	scriptPath = new URL(document.currentScript.src + '/..').href;
-	if (scriptPath.slice(-1) === '/') {
-		scriptPath = scriptPath.slice(0, -1);
-	}
-	// TODO - Figure out why 'import.meta' causes error
-// } else if(import.meta){
-// 	scriptPath = new URL(import.meta.url + "/..").href;
-// 	if (scriptPath.slice(-1) === '/') {
-// 		scriptPath = scriptPath.slice(0, -1);
-// 	}
-}else {
-	console.error('Potree was unable to find its script path using document.currentScript. Is Potree included with a script tag? Does your browser support this function?');
-}
-
+let scriptPath = document.baseURI.slice(0, -1);
 let resourcePath = scriptPath + '/app/services/potree/resources'; //TODO - This is ewy. Should be defined by project using this library instead.
 
 export {scriptPath, resourcePath};
